43fb52ad1a0e05e0fe6d86b442c6e6cbd8fe464d
ravisavant <ravisavant2022@gmail.com>
2026-02-15 22:46:52 +0530

    DatadrivenTestNG.java
    In TestNG, data-driven testing is implemented using Data Provider, where a test method executes multiple times with different sets of data

---
+++ DatadrivenTestNG.java
@@ -0,0 +1,58 @@
+// test data class
+package Testdata;
+
+import org.testng.annotations.DataProvider;
+
+public class TestData {
+
+    @DataProvider(name = "loginData")
+    public Object[][] getLoginData() {
+
+        return new Object[][] {
+            {"admin", "admin123"},
+            {"user1", "pass123"},
+            {"testuser", "test123"}
+        };
+    }
+}
+
+// testNG class
+
+package testNGsessions;
+
+import org.testng.annotations.Test;
+
+import Testdata.TestData;
+
+public class DataDrivenLoginTest {
+
+    @Test(dataProvider = "loginData", dataProviderClass = TestData.class)
+    public void loginTest(String username, String password) {
+
+        System.out.println("Username: " + username+ " | Password: " + password);
+    }
+}
+
+
+// data driven using same classs
+
+package testNGsessions;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataDrivenTest {
+
+    @DataProvider(name = "userData")
+    public Object[][] getData() {
+        return new Object[][] {
+            {"Ravi", "123"},
+            {"Ankit", "456"}
+        };
+    }
+
+    @Test(dataProvider = "userData")
+    public void testUser(String name, String id) {
+        System.out.println(name + " - " + id);
+    }
+}
